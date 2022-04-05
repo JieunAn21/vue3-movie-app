b20e3b84d4050eaab57ff634a6b3d9e5ca8826dc
JieunAn <jieun.an@waycen.com>
2022-04-05 12:14:51 +0900

    headline 추가

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Home</h1>
-  <div class="btn btn-primary">
-    HOME
-  </div>
+  <Headline />
 </template>
+
+<script>
+import Headline from '~/components/Headline'
+export default {
+    components: {
+        Headline
+    }
+}
+</script>
