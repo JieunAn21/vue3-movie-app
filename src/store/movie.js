6aea6b46f16bcb2a8fede07aff534ca5eb804f87
JieunAn <jieun.an@waycen.com>
2022-04-07 12:13:28 +0900

    영화 검색 구현

---
+++ src/store/movie.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+export default {
+  namespaced: true,
+  state: () => ({
+    movies: []
+  }),
+  getters: {},
+  mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+        console.log(state.movies)
+      })
+    },
+    resetMovies(state) {
+      state.movies = []
+    }
+  },
+  actions: {
+    async searchMovies({ commit }, payload) {
+      const { title, type, number, year } = payload
+      const OMDB_API_KEY = '7035c60c'
+
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const { Search, totalResults } = res.data
+      commit('updateState', { 
+        movies: Search
+      })
+    }
+  }
+}
