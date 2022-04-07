7474145994e6424ed8d8466436002a744e3339c8
JieunAn <jieun.an@waycen.com>
2022-04-07 12:48:15 +0900

    영화검색 추가요청 구현

--- src/store/movie.js
+++ src/store/movie.js
@@ -10,7 +10,6 @@ export default {
     updateState(state, payload) {
       Object.keys(payload).forEach(key => {
         state[key] = payload[key]
-        console.log(state.movies)
       })
     },
     resetMovies(state) {
@@ -18,7 +17,7 @@ export default {
     }
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '7035c60c'
 
@@ -27,6 +26,21 @@ export default {
       commit('updateState', { 
         movies: Search
       })
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      //추가 요청
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > (number / 10)) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
